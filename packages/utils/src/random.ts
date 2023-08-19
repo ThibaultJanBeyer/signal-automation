5c5ed5d4ec1d78966432f267eeffe7c6c8a0b24c
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-08-20 01:06:59 +0200

    add selection chance

--- packages/utils/src/random.ts
+++ packages/utils/src/random.ts
@@ -3,3 +3,11 @@ export const getRandomItemFromArray = <T>(arr: T[]): T | undefined => {
   if (arr.length === 1) return arr[0];
   return arr[Math.floor(Math.random() * arr.length)];
 };
+
+export const randomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+export const isSelected = (reference: number) => {
+  return randomInt(0, 100) <= reference;
+};
