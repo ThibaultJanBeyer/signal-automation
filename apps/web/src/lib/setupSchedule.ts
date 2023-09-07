9d768e8252d42c3cbd817cf46a5ed0878a405c64
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-09-07 20:52:35 +0100

    setup schedule

--- apps/web/src/lib/setupSchedule.ts
+++ apps/web/src/lib/setupSchedule.ts
@@ -12,52 +12,51 @@ export const setupSchedule = async () => {
   if (isRunning) return;
   isRunning = true;
 
-  try {
-    const res = await fetch(`${UPSTASH_SCHEDULES_URI}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.UPSTASH_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-    });
-    const receiveSchedules: Schedule[] = await res.json();
-    const receiveSchedulesIds = receiveSchedules
-      .filter((r) => r.destination.url == MESSAGE_RECEIVER_URL)
-      .map((r) => r.scheduleId);
+  const res = await fetch(`${UPSTASH_SCHEDULES_URI}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.UPSTASH_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+  });
+  const receiveSchedules: Schedule[] = await res.json();
+  const receiveSchedulesIds = receiveSchedules
+    .filter((r) => r.destination.url == MESSAGE_RECEIVER_URL)
+    .map((r) => r.scheduleId);
 
-    if (receiveSchedulesIds.length > 0)
-      await Promise.all(
-        receiveSchedulesIds.map(async (id, index) => {
-          const res = await fetch(`${UPSTASH_SCHEDULES_URI}/${id}`, {
-            method: "DELETE",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.UPSTASH_TOKEN}`,
-            },
-          });
-          const result = await res.json();
-          console.log(`Deleted schedule ${result.scheduleId}`);
-        }),
-      );
+  if (receiveSchedulesIds.length > 0) {
+    await Promise.all(
+      receiveSchedulesIds.map(async (id, index) => {
+        const res = await fetch(`${UPSTASH_SCHEDULES_URI}/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.UPSTASH_TOKEN}`,
+          },
+        });
+        const result = await res.json();
+        console.log(`Deleted schedule ${id}`);
+      }),
+    );
+  }
 
-    if (MESSAGE_RECEIVER_URL.includes("signal-automation-git-"))
-      throw new Error("Not on git branch urls");
-
+  if (
+    MESSAGE_RECEIVER_URL !== "https://signal-automation.vercel.app/api/receive"
+  ) {
     const resp = await fetch(`${UPSTASH_PUBLISH_URI}/${MESSAGE_RECEIVER_URL}`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${process.env.UPSTASH_TOKEN}`,
         "Content-Type": "application/json",
-        "Upstash-Cron": "*/11 * * * *",
+        "Upstash-Cron": "*/16 * * * *",
         "Upstash-Forward-Auth": process.env.SERVER_TOKEN!,
       },
       credentials: "include",
     });
     const result = await resp.json();
     console.log(`Created schedule ${result.scheduleId}`);
-  } catch (err: any) {
-    if (err.message.includes("Not on git branch urls")) console.info(err);
-    else console.error(err);
+  } else {
+    console.log(`Not creating schedule`, MESSAGE_RECEIVER_URL);
   }
 
   isRunning = false;
